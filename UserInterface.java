737c4f703eab86b45478b6de5782f9e9781ad88f
LOMI SOFTWARES <162574751+lomi-software@users.noreply.github.com>
2026-01-17 21:00:02 +0000

    Remove unused classes and interfaces related to password generation and text interface

--- UserInterface.java
+++
@@ -1,8 +0,0 @@
-public interface UserInterface {
-    public int getPassordLength();
-    public boolean addUpperCase();
-    public boolean addLowerCase();
-    public boolean addNumber();
-    public boolean addSymbols();
-    public void displayPassord(String passord);
-}
